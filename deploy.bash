99ccba70adf0d2a592c0c968e3fde2ba60daedb4
Ihar Suvorau <ihar.suvorau@gmail.com>
2022-03-10 17:10:43 +0200

    Deployment script is added

---
+++ deploy.bash
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+VERSION_RULE=$1
+
+[ -z "$VERSION_RULE" ] && echo "Version rule is empty" && exit 1
+
+poetry update
+poetry version $VERSION_RULE
+poetry publish --build
+
+git tag "v$(poetry version -s)"
+git push --tags
